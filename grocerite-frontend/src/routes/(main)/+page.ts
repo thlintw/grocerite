220b4973f85b5bc8d5b677f3dac2b0310a85f63c
THL5800X\THL <me@thlin.tw>
2024-04-09 20:42:39 +0800

    no message

--- grocerite-frontend/src/routes/(main)/+page.ts
+++ grocerite-frontend/src/routes/(main)/+page.ts
@@ -1,15 +1,7 @@
 /** @type {import('./$types').PageLoad} */
 
 export async function load({ params, fetch }) {
-    // Fetch data from an https://httpbin.org/get
-    const response = await fetch('https://httpbin.org/get');
-    const data = await response.json();
-    console.log(data);
+	
 	return {
-		post: {
-			title: `title`,
-			content: `Content ccccc`,
-            url: data.url,
-		}
 	};
 }
