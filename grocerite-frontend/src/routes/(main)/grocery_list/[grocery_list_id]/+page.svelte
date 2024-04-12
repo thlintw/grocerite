e4d88d50a05e08f07a3bc3a02ac5781eb72235db
THL5800X\THL <me@thlin.tw>
2024-04-13 00:28:17 +0800

    making a dialogue

--- grocerite-frontend/src/routes/(main)/grocery_list/[grocery_list_id]/+page.svelte
+++ grocerite-frontend/src/routes/(main)/grocery_list/[grocery_list_id]/+page.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
     import { onMount } from 'svelte';
+    import { Member } from '$lib/models/household';
     import { GroceryList, GroceryListItem } from '$lib/models/groceryList';
     import { ItemCategory, getItemCategoryIcon } from '$lib/models/item';
     import { _ } from 'svelte-i18n';
 	import { lc } from '$lib/stores/general';
     import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
-    import { faCheck } from '@fortawesome/free-solid-svg-icons';
+    import { faCheck, faEllipsisVertical, faUser, faCalendar, faBagShopping, faAsterisk, faTimes } from '@fortawesome/free-solid-svg-icons';
     import { properCapitalize } from '$lib/utilities';
-    import { fade } from 'svelte/transition';
+    import { fade, scale } from 'svelte/transition';
+    import { scaleFade } from '$lib/transitions';
 
     onMount(() => {
         console.log('onMount');
@@ -15,7 +17,12 @@
 
     const mockList = new GroceryList({
         idx: 1,
-        name: 'Sample list',
+        name: 'Sample list barstarotn aroiuftnorabh aorfuntoara aorfutnorafntora',
+        deadlineString: '2024-12-31',
+        asignee: new Member({
+            userIdx: 1,
+            name: 'Steve Jobs',
+        }),
         householdIdx: 1,
         iconIdx: 2,
         starred: true,
@@ -93,21 +100,15 @@
         ]
     });
     
-    // Assuming GroceryListItem and mockList are defined as before
-    
-    // Create an array of enum values in the order they were declared
     const categoryOrder = Object.values(ItemCategory);
 
-    // Function to get ordinal of category based on enum declaration
     const getCategoryOrdinal = (category) => categoryOrder.indexOf(category);
 
-    // Group items by category
     let groupedItems = mockList.items.reduce((acc, item) => {
         (acc[item.category] = acc[item.category] || []).push(item);
         return acc;
     }, {});
 
-    // Sort categories by enum order and items within each category by idx
     let sortedCategories = Object.keys(groupedItems).sort(
         (a, b) => getCategoryOrdinal(a) - getCategoryOrdinal(b)
     );
@@ -132,19 +133,103 @@
         }, 1000);
     };
 
+    $: tickedCount = mockList.items.filter((i) => i.ticked).length;
+    $: totalItemCount = mockList.items.length;
+
+    let showActionDialog = false;
+
+    const openActionDialog = () => {
+        showActionDialog = true;
+    };
+
+    const closeActionDialog = () => {
+        showActionDialog = false;
+    };
+
 </script>
 
-<div class="flex flex-col w-full">
-    <div class="{$lc.title} text-2xl text-orange-500">
-        {properCapitalize(mockList.name)}
+<div class="flex flex-col w-full gap-3">
+    {#if showActionDialog}
+        <button transition:fade
+            class="fixed inset-0 left-0 top-0 w-full h-full z-[11000] bg-neutral-700/20" 
+            on:click={closeActionDialog}></button>
+        <div transition:scaleFade
+            class="fixed top-0 right-0 bottom-0 left-0 z-[11001] pointer-events-none 
+                flex items-center justify-center">
+            <div class="pointer-events-auto z-[11002]flex flex-col
+                ">
+                <div class="ml-1 text-2xl text-orange-500 flex-grow {$lc.title} 
+                    relative drop-shadow-grocerite-orange-100-lg top-4 left-1">
+                    {$_('common_actions')}
+                </div>
+                <div class="bg-orange-50 rounded-xl flex flex-col gap-3 px-3 py-3 pt-4
+                    shadow-grocerite-orange-200-sm">
+                    <div class="hover:bg-orange-100 rounded-md">
+                        <button type="button" class="w-full flex text-orange-500 {$lc.text} text-xl">
+                            {$_('groceryList_completeThisList')}
+                        </button>
+                    </div>
 
+                </div>
+            </div>
+        </div>
+    {/if}
+
+    <div class="{$lc.title} text-2xl text-orange-500 flex items-center">
+        {#if mockList.starred}
+            <div class="text-yellow-400 text-3xl mr-2">
+                <FontAwesomeIcon icon={faAsterisk} />
+            </div>
+        {/if}
+        <span class="whitespace-nowrap overflow-hidden text-ellipsis">{properCapitalize(mockList.name)}</span>
+        <button type="button" class="ml-auto"
+            on:click={openActionDialog}>
+            <FontAwesomeIcon icon={faEllipsisVertical} class="w-6 h-6"/>
+        </button>
+    </div>
+    <div class="flex text-sm gap-3 {$lc.text}">
+        {#if mockList.asignee}
+            <div class="bg-orange-50 rounded-md px-2 py-0.5 flex gap-1 text-base lg:text-lg items-center">
+                <div class="text-lime-600">
+                    <FontAwesomeIcon
+                        icon={faUser}
+                        class="text-lime-600"
+                    />
+                </div>
+                <div class="text-neutral-800">
+                    {mockList.asignee.name}
+                </div>
+            </div>
+        {/if}
+        <div class="bg-orange-50 rounded-md px-2 py-0.5 flex gap-1 text-base lg:text-lg items-center">
+            <div class="text-lime-600">
+                <FontAwesomeIcon
+                    icon={faCalendar}
+                    class="text-lime-600"
+                />
+            </div>
+            <div class="text-neutral-800">
+                2024-12-31
+            </div>
+        </div>
+        <div class="bg-orange-50 rounded-md px-2 py-0.5 flex gap-1 text-base lg:text-lg items-center">
+            <div class="text-lime-600">
+                <FontAwesomeIcon
+                    icon={faBagShopping}
+                    class="text-lime-600"
+                />
+            </div>
+            <div class="text-neutral-800">
+                {tickedCount.toLocaleString()} / {totalItemCount.toLocaleString()}
+            </div>
+        </div>
     </div>
     
     <div class="
         flex flex-col
         w-full h-full gap-5
         pb-32 lg:pb-3
-        mt-5
+        mt-2
     ">
         {#each sortedCategories as category}
             <div class="w-full bg-orange-50 px-3 lg:px-5 py-3 flex flex-col gap-3 rounded-xl shadow-grocerite-orange-200-sm">
@@ -161,7 +246,7 @@
                             {#if apiLoading.includes(item.idx)}
                                 <div 
                                     class="animate-spin rounded-full h-4 w-4 border-2 border-b-transparent border-orange-200"></div>
-                            {:else if item.ticked && !apiLoading.includes(item.idx)}
+                            {:else if tickedItemIdxs.includes(item.idx) && !apiLoading.includes(item.idx)}
                                 <div class="absolute -top-[2px] -left-[2px] w-9 h-9 text-lime-600"
                                 transition:fade>
                                     <FontAwesomeIcon 
@@ -172,9 +257,13 @@
                             {/if}
                         </button>
                         <div class="text-xl {$lc.text} font-mono text-center w-16 font-bold shrink-0">{item.quantity}</div>
-                        <div class="{$lc.text} text-base lg:text-lg overflow-hidden text-ellipsis text-nowrap pr-2
-                            {tickedItemIdxs.includes(item.idx) ? 'line-through text-neutral-500' : ''}
-                        ">{item.name}  </div>
+                        <div class="{$lc.text} text-base lg:text-lg overflow-hidden text-ellipsis text-nowrap pr-2">
+                            {#if tickedItemIdxs.includes(item.idx)}
+                                <span class="line-through text-neutral-400">{item.name}</span>
+                            {:else}
+                                <span class="">{item.name}</span>
+                            {/if}
+                        </div>
                     </div>
                 {/each}
                 </div>
