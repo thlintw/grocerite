6dfb641af10d85a7feda5be4490a4fc7a4cb9289
TH Lin <me@thlin.tw>
2024-05-15 08:56:35 +0800

    new arg parser

--- grocerite-backend/run.py
+++ grocerite-backend/run.py
@@ -1,19 +1,26 @@
 import sys, os
 from app import create_app, db
 from dotenv import load_dotenv, find_dotenv
+import argparse
 
-env_arg = 'dev' 
-if len(sys.argv) > 1:
-    env_arg = sys.argv[1]
 
-initdb = ''
-if len(sys.argv) > 2:
-    initdb = sys.argv[2]
+parser = argparse.ArgumentParser(
+    description='Run the application with specified environment.'
+)
+parser.add_argument(
+    '--env',
+    choices=['dev', 'prod'],
+    default='dev',
+    help='Set the environment (default: dev)'
+)
+parser.add_argument(
+    '--initdb',
+    action='store_true',
+    help='Initialize the database'
+)
 
-env_path = '.env.dev'
-if env_arg == 'prod':
-    env_path = '.env'
-
+args = parser.parse_args()
+env_path = '.env.dev' if args.env == 'dev' else '.env'
 
 env_file_path = find_dotenv(env_path)
 if not env_file_path:
@@ -26,11 +33,11 @@ app = create_app()
 
 if __name__ == "__main__":
 
-    if initdb == 'initdb':
+    if args.initdb:
         with app.app_context():
             db.create_all()
             print('Initialized the database.')
     else:
-        print(f'Running in {env_arg} mode')
+        print(f'Running in {args.env} mode')
         debug = True if os.getenv('APP_MODE', 'dev') == 'dev' else False
         app.run(debug=debug, port=8765)
